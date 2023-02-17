046348ec9c614f27ee8620234ed5464192972fed
rusokverse <rusokverse@MACBOOKPRO>
2023-02-16 21:36:52 -0400

    Estructuras de datos: Pila, Cola; ToString y Métodos estáticos.

--- EjemploAplicacionVentana/Form2.Designer.cs
+++ EjemploAplicacionVentana/Form2.Designer.cs
@@ -32,18 +32,49 @@
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.est_dat_3 = new System.Windows.Forms.TabPage();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txt_salida = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.button2 = new System.Windows.Forms.Button();
             this.txt_elemento = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.est_dat_1 = new System.Windows.Forms.TabPage();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.lbl_mostrar = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.lbl_elemento_pop = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.txt_ingreso = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.est_dat_2 = new System.Windows.Forms.TabPage();
-            this.txt_salida = new System.Windows.Forms.Label();
+            this.groupBox8 = new System.Windows.Forms.GroupBox();
+            this.lbl_mostrar_queue = new System.Windows.Forms.Label();
+            this.groupBox7 = new System.Windows.Forms.GroupBox();
+            this.lbl_mostrar_dequeue = new System.Windows.Forms.Label();
+            this.button8 = new System.Windows.Forms.Button();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.txt_direccion = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_edad = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_nombre = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
             this.est_dat_3.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox1.SuspendLayout();
+            this.est_dat_1.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.est_dat_2.SuspendLayout();
+            this.groupBox8.SuspendLayout();
+            this.groupBox7.SuspendLayout();
+            this.groupBox6.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
@@ -88,6 +119,15 @@
             this.groupBox2.TabIndex = 4;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Salida";
+            // 
+            // txt_salida
+            // 
+            this.txt_salida.AutoSize = true;
+            this.txt_salida.Location = new System.Drawing.Point(34, 73);
+            this.txt_salida.Name = "txt_salida";
+            this.txt_salida.Size = new System.Drawing.Size(38, 15);
+            this.txt_salida.TabIndex = 1;
+            this.txt_salida.Text = "label2";
             // 
             // button3
             // 
@@ -139,6 +179,9 @@
             // 
             // est_dat_1
             // 
+            this.est_dat_1.Controls.Add(this.groupBox5);
+            this.est_dat_1.Controls.Add(this.groupBox4);
+            this.est_dat_1.Controls.Add(this.groupBox3);
             this.est_dat_1.Location = new System.Drawing.Point(4, 24);
             this.est_dat_1.Name = "est_dat_1";
             this.est_dat_1.Padding = new System.Windows.Forms.Padding(3);
@@ -147,8 +190,107 @@
             this.est_dat_1.Text = "Stack";
             this.est_dat_1.UseVisualStyleBackColor = true;
             // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.lbl_mostrar);
+            this.groupBox5.Controls.Add(this.button6);
+            this.groupBox5.Location = new System.Drawing.Point(460, 31);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(200, 172);
+            this.groupBox5.TabIndex = 2;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "MOSTRAR";
+            // 
+            // lbl_mostrar
+            // 
+            this.lbl_mostrar.AutoSize = true;
+            this.lbl_mostrar.Location = new System.Drawing.Point(20, 66);
+            this.lbl_mostrar.Name = "lbl_mostrar";
+            this.lbl_mostrar.Size = new System.Drawing.Size(0, 15);
+            this.lbl_mostrar.TabIndex = 1;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(42, 26);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 0;
+            this.button6.Text = "Mostrar";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.lbl_elemento_pop);
+            this.groupBox4.Controls.Add(this.button5);
+            this.groupBox4.Location = new System.Drawing.Point(234, 28);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(200, 166);
+            this.groupBox4.TabIndex = 1;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "SALIDA";
+            // 
+            // lbl_elemento_pop
+            // 
+            this.lbl_elemento_pop.AutoSize = true;
+            this.lbl_elemento_pop.Location = new System.Drawing.Point(30, 92);
+            this.lbl_elemento_pop.Name = "lbl_elemento_pop";
+            this.lbl_elemento_pop.Size = new System.Drawing.Size(0, 15);
+            this.lbl_elemento_pop.TabIndex = 1;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(24, 47);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "POP";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button4);
+            this.groupBox3.Controls.Add(this.txt_ingreso);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Location = new System.Drawing.Point(21, 18);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 185);
+            this.groupBox3.TabIndex = 0;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "INGRESO";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(17, 113);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "PUSH";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // txt_ingreso
+            // 
+            this.txt_ingreso.Location = new System.Drawing.Point(18, 63);
+            this.txt_ingreso.Name = "txt_ingreso";
+            this.txt_ingreso.Size = new System.Drawing.Size(100, 23);
+            this.txt_ingreso.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Ingrese un numero:";
+            // 
             // est_dat_2
             // 
+            this.est_dat_2.Controls.Add(this.groupBox8);
+            this.est_dat_2.Controls.Add(this.groupBox7);
+            this.est_dat_2.Controls.Add(this.groupBox6);
             this.est_dat_2.Location = new System.Drawing.Point(4, 24);
             this.est_dat_2.Name = "est_dat_2";
             this.est_dat_2.Padding = new System.Windows.Forms.Padding(3);
@@ -157,14 +299,127 @@
             this.est_dat_2.Text = "Queue";
             this.est_dat_2.UseVisualStyleBackColor = true;
             // 
-            // txt_salida
+            // groupBox8
             // 
-            this.txt_salida.AutoSize = true;
-            this.txt_salida.Location = new System.Drawing.Point(34, 73);
-            this.txt_salida.Name = "txt_salida";
-            this.txt_salida.Size = new System.Drawing.Size(38, 15);
-            this.txt_salida.TabIndex = 1;
-            this.txt_salida.Text = "label2";
+            this.groupBox8.Controls.Add(this.lbl_mostrar_queue);
+            this.groupBox8.Location = new System.Drawing.Point(444, 14);
+            this.groupBox8.Name = "groupBox8";
+            this.groupBox8.Size = new System.Drawing.Size(200, 220);
+            this.groupBox8.TabIndex = 2;
+            this.groupBox8.TabStop = false;
+            this.groupBox8.Text = "MOSTRAR";
+            // 
+            // lbl_mostrar_queue
+            // 
+            this.lbl_mostrar_queue.AutoSize = true;
+            this.lbl_mostrar_queue.Location = new System.Drawing.Point(16, 29);
+            this.lbl_mostrar_queue.Name = "lbl_mostrar_queue";
+            this.lbl_mostrar_queue.Size = new System.Drawing.Size(38, 15);
+            this.lbl_mostrar_queue.TabIndex = 0;
+            this.lbl_mostrar_queue.Text = "label7";
+            // 
+            // groupBox7
+            // 
+            this.groupBox7.Controls.Add(this.lbl_mostrar_dequeue);
+            this.groupBox7.Controls.Add(this.button8);
+            this.groupBox7.Location = new System.Drawing.Point(233, 14);
+            this.groupBox7.Name = "groupBox7";
+            this.groupBox7.Size = new System.Drawing.Size(200, 220);
+            this.groupBox7.TabIndex = 1;
+            this.groupBox7.TabStop = false;
+            this.groupBox7.Text = "SALIDA";
+            // 
+            // lbl_mostrar_dequeue
+            // 
+            this.lbl_mostrar_dequeue.AutoSize = true;
+            this.lbl_mostrar_dequeue.Location = new System.Drawing.Point(22, 76);
+            this.lbl_mostrar_dequeue.Name = "lbl_mostrar_dequeue";
+            this.lbl_mostrar_dequeue.Size = new System.Drawing.Size(0, 15);
+            this.lbl_mostrar_dequeue.TabIndex = 1;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(56, 25);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 0;
+            this.button8.Text = "DEQUEUE";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Controls.Add(this.button7);
+            this.groupBox6.Controls.Add(this.txt_direccion);
+            this.groupBox6.Controls.Add(this.label5);
+            this.groupBox6.Controls.Add(this.txt_edad);
+            this.groupBox6.Controls.Add(this.label4);
+            this.groupBox6.Controls.Add(this.txt_nombre);
+            this.groupBox6.Controls.Add(this.label3);
+            this.groupBox6.Location = new System.Drawing.Point(12, 14);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(200, 220);
+            this.groupBox6.TabIndex = 0;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "INGRESO";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(88, 171);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "ENQUEUE";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // txt_direccion
+            // 
+            this.txt_direccion.Location = new System.Drawing.Point(82, 107);
+            this.txt_direccion.Name = "txt_direccion";
+            this.txt_direccion.Size = new System.Drawing.Size(100, 23);
+            this.txt_direccion.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 115);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 15);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Dirección:";
+            // 
+            // txt_edad
+            // 
+            this.txt_edad.Location = new System.Drawing.Point(75, 66);
+            this.txt_edad.Name = "txt_edad";
+            this.txt_edad.Size = new System.Drawing.Size(100, 23);
+            this.txt_edad.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 70);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 15);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Edad:";
+            // 
+            // txt_nombre
+            // 
+            this.txt_nombre.Location = new System.Drawing.Point(74, 29);
+            this.txt_nombre.Name = "txt_nombre";
+            this.txt_nombre.Size = new System.Drawing.Size(100, 23);
+            this.txt_nombre.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 15);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Nombre:";
             // 
             // Form2
             // 
@@ -181,6 +436,20 @@
             this.groupBox2.PerformLayout();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.est_dat_1.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.est_dat_2.ResumeLayout(false);
+            this.groupBox8.ResumeLayout(false);
+            this.groupBox8.PerformLayout();
+            this.groupBox7.ResumeLayout(false);
+            this.groupBox7.PerformLayout();
+            this.groupBox6.ResumeLayout(false);
+            this.groupBox6.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -198,5 +467,28 @@
         private System.Windows.Forms.TextBox txt_elemento;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label txt_salida;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.Label lbl_mostrar;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label lbl_elemento_pop;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox txt_ingreso;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox8;
+        private System.Windows.Forms.Label lbl_mostrar_queue;
+        private System.Windows.Forms.GroupBox groupBox7;
+        private System.Windows.Forms.Label lbl_mostrar_dequeue;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TextBox txt_direccion;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_edad;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_nombre;
+        private System.Windows.Forms.Label label3;
     }
 }
--- EjemploAplicacionVentana/Form2.cs
+++ EjemploAplicacionVentana/Form2.cs
@@ -12,6 +12,8 @@ namespace EjemploAplicacionVentana
     {
         Form1 ventana_login;
         List<string> lista_de_palabras;
+        Stack<int> pila_numeros;
+        Queue<Persona> cola_personas;
         public Form2()
         {
             InitializeComponent();
@@ -20,8 +22,8 @@ namespace EjemploAplicacionVentana
             InitializeComponent();
             ventana_login = v_login;
             lista_de_palabras = new List<string>();
-            
-            
+            pila_numeros = new Stack<int>();// Last In, First Out (LIFO)
+            cola_personas = new Queue<Persona>();
         }
         private void button1_Click(object sender, EventArgs e)
         {
@@ -45,5 +47,74 @@ namespace EjemploAplicacionVentana
                 txt_salida.Text += val_auxiliar + "\n";
             }
         }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            pila_numeros.Push(int.Parse(txt_ingreso.Text));
+            txt_ingreso.Text = "";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            lbl_elemento_pop.Text= pila_numeros.Pop().ToString();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            lbl_mostrar.Text = "";
+            foreach (int elemento in pila_numeros)
+                lbl_mostrar.Text += elemento + "\n";
+            
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            cola_personas.Enqueue(
+                new Persona(
+                    txt_nombre.Text,
+                    int.Parse(txt_edad.Text),
+                    txt_direccion.Text)
+            );
+            mostrar_personas();
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if(cola_personas.Count > 0)
+                lbl_mostrar_dequeue.Text = cola_personas.Dequeue() + "";
+            mostrar_personas();
+        }
+        private void mostrar_personas() {
+            lbl_mostrar_queue.Text = "";
+            foreach (Persona elemento in cola_personas)
+                lbl_mostrar_queue.Text += elemento + "\n";
+        }
     }
+
+    public class Persona {
+        public string nombre, direccion;
+        public int edad;
+        public Persona(string nombre, int edad, string direccion) {
+            this.nombre = nombre;
+            this.edad = edad;
+            this.direccion = direccion;
+        }
+        public override string ToString()
+        {
+            Math.Abs(123);
+            Matematica.Absoluto(123);
+            return $"Nombre: {nombre}; Edad: {edad}; Dir.: {direccion}";
+        }
+    }
+
+    public class Matematica {
+        public static Absoluto(int x) {
+            if (x >= 0)
+                return x;
+            else
+                return -x;
+        }
+    }
+
 }
